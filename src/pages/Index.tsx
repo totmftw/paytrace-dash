3531efb4e20be4b3f456ae645f9cd9e952656b3a
totmftw <ecom@totm.in>
2025-01-29 13:34:26 +0530

    theme and shit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,6 @@
 // Update this page (the content is just a fallback if you fail to update the page)
 
+
 const Index = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
